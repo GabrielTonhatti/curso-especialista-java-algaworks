377b32786fdab076fabf0d145ecf0f067e4d9717
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-07-19 20:04:12 -0300

    Comentários

--- 2-fundamentos-linguagem-java/OlaMergulhador.java
+++ 2-fundamentos-linguagem-java/OlaMergulhador.java
@@ -1,7 +1,14 @@
-public class OlaMergulhador {
+// Você pode comentar uma linha inteira
+public class OlaMergulhador { // ou na frente de uma declaração ou instrução
+    
+    /*
+        Método principal do programa.
+        Ao executar o programa, esse método será chamado automaticamente.
+    */
     
     public static void main(String[] args) {
+        // imprime a mensagem na saída padrão
         System.out.println("Olá, mergulhador!");
     }
-
+    
 }
