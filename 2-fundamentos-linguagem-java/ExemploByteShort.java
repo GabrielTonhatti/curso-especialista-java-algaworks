aa90da006ce81d6545bebabd84e0dc7303978ea4
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-07-19 22:13:16 -0300

    Byte e Short

---
+++ 2-fundamentos-linguagem-java/ExemploByteShort.java
@@ -0,0 +1,11 @@
+public class ExemploByteShort {
+    
+    public static void main(String[] args) {
+        byte idade = 127; // Valor máximo
+        // byte idade2 = 128; // Não compila
+        
+        short quatidadeEstoque = 32767; // Valor máximo
+        // short quatidadeEstoque2 = 32768; // Não compila
+    }
+    
+}
---
+++ 2-fundamentos-linguagem-java/ExemploChar.java
@@ -0,0 +1,14 @@
+public class ExemploChar {
+    
+    public static void main(String[] args) {
+        // char inicialDoNome = "G"; // Não compila
+        char inicialDoNome = 'G';
+        char tipoCliente = '2';
+        char simbolo = '@';
+        
+        System.out.println(inicialDoNome);
+        System.out.println(tipoCliente);
+        System.out.println(simbolo);
+    }
+    
+}
