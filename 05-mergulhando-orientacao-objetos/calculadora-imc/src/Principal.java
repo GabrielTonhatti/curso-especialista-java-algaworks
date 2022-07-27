f40e6384844c889bc2d63f428d8c16c45e3c50b2
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-07-27 08:46:18 -0300

    Passando objetos como argumentos de métodos

--- 05-mergulhando-orientacao-objetos/calculadora-imc/src/Principal.java
+++ 05-mergulhando-orientacao-objetos/calculadora-imc/src/Principal.java
@@ -2,10 +2,12 @@ public class Principal {
 
     public static void main(String[] args) {
         CalculadoraImc calculadoraImc = new CalculadoraImc();
-        double altura = 1.82;
-        double peso = 175;
 
-        IndiceMassaCorporal imc = calculadoraImc.calcular(peso, altura);
+        Paciente joao = new Paciente();
+        joao.altura = 1.82;
+        joao.peso = 175;
+
+        IndiceMassaCorporal imc = calculadoraImc.calcular(joao);
 
         if (imc.estaComObesidade()) {
             System.out.printf("Paciente com altura %.2f e peso de %.2f está com obesidade%n", imc.altura, imc.peso);
