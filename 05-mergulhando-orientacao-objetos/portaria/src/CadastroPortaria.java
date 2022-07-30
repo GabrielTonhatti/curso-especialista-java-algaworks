96ae06a80d71821266e4c271314252c8de39605e
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-07-29 21:11:04 -0300

    Sobrecarga de métodos

--- 05-mergulhando-orientacao-objetos/portaria/src/CadastroPortaria.java
+++ 05-mergulhando-orientacao-objetos/portaria/src/CadastroPortaria.java
@@ -1,5 +1,15 @@
 public class CadastroPortaria {
 
+    static final int TEMPO_EXPIRACAO_PADRAO_EM_MESES = 1;
+
+    void cadastrar(final Visitante visitante) {
+        this.cadastrar(visitante, TEMPO_EXPIRACAO_PADRAO_EM_MESES);
+    }
+
+    void cadastrar(final Visitante visitante, final long senha) {
+
+    }
+
     void cadastrar(final Visitante visitante, final int tempoExpiracaoEmMeses) {
         final int tempoExpiracaoEmDias = tempoExpiracaoEmMeses * 30;
 
