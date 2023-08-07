aa05d79131ad3b3081bb12462a8c8757eb483c0f
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-08-07 16:06:03 -0300

    Código Limpo: pensando melhor nos argumentos de métodos

--- 06-boas-praticas-effective-java-clean-code/boas-praticas-clean-code/src/GatewayPagamento.java
+++
@@ -1,6 +0,0 @@
-public class GatewayPagamento {
-
-    public static boolean autorizarPagamento(String numeroCartao, double valorTotal) {
-        return true;
-    }
-}
--- 06-boas-praticas-effective-java-clean-code/boas-praticas-clean-code/src/Produto.java
+++ 06-boas-praticas-effective-java-clean-code/boas-praticas-clean-code/src/Produto.java
@@ -1,4 +1,26 @@
 public class Produto {
 
+    String nome;
+    boolean ativo;
     double precoUnitario;
+
+    void alterarStatus(boolean ativo) {
+        if (ativo) {
+            System.out.println("Ativando");
+        } else {
+            System.out.println("Desativando");
+        }
+
+        this.ativo = ativo;
+    }
+
+    void ativar() {
+        System.out.println("Ativando");
+        this.ativo = true;
+    }
+
+    void inativar() {
+        System.out.println("Desativando");
+        this.ativo = false;
+    }
 }
