b51f57e7fb441538b44aa6c1d3e50031103de9f8
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-08-08 14:59:00 -0300

    Autoboxing e unboxing

--- 07-wrappers-boxing/wrappers-boxing/src/Principal.java
+++ 07-wrappers-boxing/wrappers-boxing/src/Principal.java
@@ -1,15 +1,7 @@
 public class Principal {
 
     public static void main(String[] args) {
-        int idade = 20;
-        short idadeShort = (short) idade;
-
-        Integer diasEntrega = Integer.valueOf(30);
-        Short diasEntregaShort = Short.valueOf(diasEntrega.shortValue());
-        Long diasEntregaLong = Long.valueOf(diasEntrega.longValue());
-
-        Double valoTotal = Double.valueOf(1500.2);
-        int valorTotalInt = valoTotal.intValue();
-        System.out.println(valorTotalInt);
+        Integer diasEntrega = 30;
+        int diasEntregaInt = diasEntrega;
     }
 }
