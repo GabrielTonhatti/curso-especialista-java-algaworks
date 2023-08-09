bea5f5bee3c4fa4f24a7bb2bcab899109df0571d
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-08-09 16:07:45 -0300

    Copiando e expandindo arrays

--- 08-trabalhando-com-arrays/arrays/src/Turma.java
+++ 08-trabalhando-com-arrays/arrays/src/Turma.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
+
 public class Turma {
 
     String identificacao;
     String nomeProfessora;
-    Aluno[] alunos;
+    Aluno[] alunos = new Aluno[0];
+
+    void adicionarAluno(Aluno aluno) {
+        alunos = Arrays.copyOf(alunos, alunos.length + 1);
+        alunos[alunos.length - 1] = aluno;
+    }
 
     void imprimirListaDeAlunos() {
         for (Aluno aluno : alunos) {
