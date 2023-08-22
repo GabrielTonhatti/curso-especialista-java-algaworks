ba7f949749ff7a6a78050d55345b03c4d250014d
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-08-22 16:49:29 -0300

    Boas práticas: use varargs com cuidado

--- 08-trabalhando-com-arrays/arrays/src/Principal.java
+++ 08-trabalhando-com-arrays/arrays/src/Principal.java
@@ -6,17 +6,6 @@ public class Principal {
         fatura.valorTotal = 1_293.55;
 
         ServicoCobranca servicoCobranca = new ServicoCobranca();
-
-//        String[] emailsCobranca = {"joao@algaworks.com", "maria@algaworks.com"};
-//        servicoCobranca.pagar(fatura, emailsCobranca);
-
-//        servicoCobranca.pagar(fatura, new String[]{"joao@algaworks.com", "maria@algaworks.com"});
-
-//        servicoCobranca.pagar(fatura, new String[0]);
-//        servicoCobranca.pagar(fatura, new String[]{});
-
-//        servicoCobranca.pagar(fatura, "joao@algaworks.com", "maria@algaworks.com");
-
-        servicoCobranca.pagar(fatura);
+        servicoCobranca.pagar(fatura, "joao@algaworks.com", "maria@algaworks.com");
     }
 }
--- 08-trabalhando-com-arrays/arrays/src/ServicoCobranca.java
+++ 08-trabalhando-com-arrays/arrays/src/ServicoCobranca.java
@@ -1,10 +1,21 @@
+import java.util.Objects;
+
 public class ServicoCobranca {
 
-    void pagar(Fatura fatura, String... emailsCobranca) {
+    void pagar(Fatura fatura, String emailCobranca, String... emailsAdicionais) {
+        Objects.requireNonNull(fatura, "Informe a fatura");
+        Objects.requireNonNull(emailCobranca, "Informe o e-mail de cobrança");
+
         System.out.printf("Fatura %d, no valor total de R$%.2f, foi paga.%n", fatura.numero, fatura.valorTotal);
 
-        for (String email : emailsCobranca) {
-            System.out.printf("Fatura %d enviada para %s.%n", fatura.numero, email);
+        enviarNotificacao(fatura, emailCobranca);
+
+        for (String email : emailsAdicionais) {
+            enviarNotificacao(fatura, email);
         }
+    }
+
+    void enviarNotificacao(Fatura fatura, String email) {
+        System.out.printf("Fatura %d enviada para %s.%n", fatura.numero, email);
     }
 }
